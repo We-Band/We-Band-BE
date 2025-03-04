ec22d0f17f9b6f97e64c3b53fb7690332c00913b
hotchillbread <kimbap0114@gmail.com>
2025-03-04 15:02:27 +0900

    refactor: 가입여부 확인하는 미들웨어userController에서 분리

---
+++ src/middlewares/userMiddlewares.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { logger } from "../utils/logger.js";
+
+const prisma = new PrismaClient();
+
+/**이미 회원가입 여부 검증 */
+export const isJoined = async (req, res, next) => {
+    try {
+        const { email } = req.body; 
+        
+        if (!email) {
+            logger.error("필수 입력값이 없습니다.");
+            return res.status(400).json({ message: "필수 입력값이 없습니다." });  
+        }
+
+        const existingUser = await prisma.weBandUser.findUnique({
+            where: { email },
+        });
+
+        if (existingUser) {
+            return res.status(409).json({ message: "이미 가입된 이메일입니다." });
+        }  
+
+        logger.info("회원 가입 여부 검증 완료", { email });
+        next(); //
+    } catch (error) {
+        logger.error(`회원 가입 여부 검증 실패: ${error.message}`, { error });
+        return res.status(500).json({ message: "서버 오류 발생" });
+    }
+};
