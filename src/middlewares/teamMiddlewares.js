7caa4650e222210f7b50126625e9ddf1c93730a6
ymj10003 <ymj10003@gmail.com>
2025-03-22 02:34:16 +0900

    팀 생성, 삭제, 전체 팀 조회, 내 팀 조회, 팀원 추가, 삭제 구현

--- src/middlewares/teamMiddlewares.js
+++ src/middlewares/teamMiddlewares.js
@@ -3,31 +3,67 @@ import { logger } from "../utils/logger.js";
 
 const prisma = new PrismaClient();
 
-//팀장 여부 확인
+//팀장 or 회장 여부 확인
 
 export const isTeamLeader = async (req, res, next) => {
-    try {
-        const userId = req.user.user_id;
-        const teamId = req.params.teamId;
+  try {
+    const userId = req.user.user_id;
+    const teamId = req.params.teamId;
+    const clubId = req.params.clubId;
 
-        const TeamLeader = await prisma.Team.findFirst({
-            where : {
-                team_id: Number(teamId),
-                created_by: Number(userId)
-            }
-        });
+    const TeamLeader = await prisma.Team.findFirst({
+      where: {
+        team_id: Number(teamId),
+        created_by: Number(userId),
+      },
+    });
 
-        if(!TeamLeader){
-            logger.info("해당 팀의 팀장이 아닙니다!");
-            return res.status(409).json({message: "팀장만이 사용할 수 있는 기능입니다."});
-        }
+    const ClubLeader = await prisma.Club.findFirst({
+      where: {
+        club_id: Number(clubId),
+        club_leader: Number(userId),
+      },
+    });
 
-        logger.info("팀장 검증 완료");
-        next();
-
+    if (!TeamLeader && !ClubLeader) {
+      logger.info("해당 팀의 팀장이 아닙니다!");
+      return res
+        .status(409)
+        .json({ message: "팀장만이 사용할 수 있는 기능입니다." });
     }
-    catch(error){
-        logger.error(`팀장 검증 실패${error.message}`, { error } );
-        return res.status(500).json({ message: "서버 오류 발생" });
+
+    logger.info("팀장 검증 완료");
+    next();
+  } catch (error) {
+    logger.error(`팀장 검증 실패${error.message}`, { error });
+    return res.status(500).json({ message: "서버 오류 발생" });
+  }
+};
+
+// 팀 소속 여부 확인
+export const isTeamMember = async (req, res, next) => {
+  try {
+    const userId = req.user.user_id;
+    const teamId = req.params.teamId;
+
+    const TeamMember = await prisma.TeamMember.findFirst({
+      where: {
+        team_id: Number(teamId),
+        user_id: Number(userId),
+      },
+    });
+
+    if (!TeamMember) {
+      logger.info("해당 팀의 팀원이 아닙니다!");
+      return res
+        .status(409)
+        .json({ message: "팀원만이 사용할 수 있는 기능입니다." });
     }
-}
+
+    logger.info("팀원 검증 완료");
+    next();
+  } catch (error) {
+    logger.error(`팀원 검증 실패${error.message}`, { error });
+    return res.status(500).json({ message: "서버 오류 발생" });
+  }
+};
--- src/services/jwtServices.js
+++ src/services/jwtServices.js
@@ -1,13 +1,17 @@
-import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
-import { logger } from '../utils/logger.js';
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import { logger } from "../utils/logger.js";
 
 const prisma = new PrismaClient();
 
 // JWT 액세스 토큰 생성
 export const generateAccessToken = (user) => {
   return jwt.sign(
-    { userId: user.user_id, email: user.email, kakaoID: user.kakao_id.toString() },
+    {
+      userId: user.user_id,
+      email: user.email,
+      kakaoID: user.kakao_id.toString(),
+    },
     process.env.JWT_SECRET,
     { expiresIn: process.env.JWT_ACCESS_EXPIRATION }
   );
@@ -16,7 +20,11 @@ export const generateAccessToken = (user) => {
 // JWT 리프래시 토큰 생성
 export const generateRefreshToken = (user) => {
   return jwt.sign(
-    { userId: user.user_id, email: user.email, kakaoID: user.kakao_id.toString() },
+    {
+      userId: user.user_id,
+      email: user.email,
+      kakaoID: user.kakao_id.toString(),
+    },
     process.env.JWT_REFRESH_SECRET,
     { expiresIn: process.env.JWT_REFRESH_EXPIRATION }
   );
@@ -27,7 +35,7 @@ export const refreshAccessToken = async (req) => {
   try {
     const refreshToken = req.cookies.refreshToken;
     if (!refreshToken) {
-      throw new Error('Refresh Token이 필요합니다.');
+      throw new Error("Refresh Token이 필요합니다.");
     }
     // 리프래시 토큰 검증
     const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
@@ -36,15 +44,17 @@ export const refreshAccessToken = async (req) => {
     });
 
     if (!user) {
-      throw new Error('사용자를 찾을 수 없습니다.');
+      throw new Error("사용자를 찾을 수 없습니다.");
     }
 
     // 새로운 액세스 토큰 발급
     const newAccessToken = generateAccessToken(user);
-    logger.info(`Refresh Token 검증 성공 - 새로운 Access Token 발급: ${user.email}`);
+    logger.info(
+      `Refresh Token 검증 성공 - 새로운 Access Token 발급: ${user.email}`
+    );
     return newAccessToken;
   } catch (err) {
-    logger.error('Refresh Token 검증 실패: ' + err.message);
-    throw new Error('유효하지 않은 Refresh Token입니다.');
+    logger.error("Refresh Token 검증 실패: " + err.message);
+    throw new Error("유효하지 않은 Refresh Token입니다.");
   }
 };
