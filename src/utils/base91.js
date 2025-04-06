c4da4296e3da5e80467dcad1507af8caac38d5fb
hotchillbread <kimbap0114@gmail.com>
2025-04-06 21:15:19 +0900

    feat:base91인코딩 함수

---
+++ src/utils/base91.js
@@ -0,0 +1,35 @@
+const base91chars =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~"';
+
+export const encodeBase91 = (inputBuffer) => {
+  let b = 0,
+    n = 0,
+    output = "";
+
+  for (let i = 0; i < inputBuffer.length; i++) {
+    b |= inputBuffer[i] << n;
+    n += 8;
+
+    if (n > 13) {
+      let v = b & 8191;
+      if (v > 88) {
+        b >>= 13;
+        n -= 13;
+      } else {
+        v = b & 16383;
+        b >>= 14;
+        n -= 14;
+      }
+      output += base91chars[v % 91] + base91chars[Math.floor(v / 91)];
+    }
+  }
+
+  if (n) {
+    output += base91chars[b % 91];
+    if (n > 7 || b > 90) {
+      output += base91chars[Math.floor(b / 91)];
+    }
+  }
+
+  return output;
+};
